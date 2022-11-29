c76106a0018335a4728c30fa8ece87bda343c2fe
smc2315 <choeyongtae085@gmail.com>
2022-11-29 15:33:05 +0900

    fix: bug

--- MoveImage.py
+++ MoveImage.py
@@ -1,37 +1,4 @@
-import cv2 as cv
-import numpy as np
-import imutils
-import matplotlib.pyplot as plt
 
-
-
-# cv2_image = cv.imread('C:/Users/yt/Desktop/20220908163000/DJI_20220908160357_0001_W.JPG', cv.IMREAD_COLOR)
-# M = np.float32([[1, 0, 25],
-#                 [0, 1, 50]])
-# shifted = cv.warpAffine(cv2_image, M, (cv2_image.shape[1], cv2_image.shape[0]))
-#cv2_image = cv.imread('C:/Users/yt/capstone/yolov5/data/images/zidane.jpg')
-# def move_image(img,x1,y1,x2,y2,dir):
-#     dx = [10,-10,0,0]
-#     dy = [0,0,10,-10]
-#     nx1 = x1+dx[dir]
-#     nx2 = x2+dx[dir]
-#     ny1 = y1+dy[dir]
-#     ny2 = y2 + dy[dir]
-#     move_area = img[y1:y2, x1:x2]
-#     img[ny1:ny2, nx1:nx2] = move_area
-#
-#     if dir==0:
-#         img[y1:y2,x1:nx1] = 0
-#     elif dir ==1:
-#         img[y1:y2, nx2:x2] = 0
-#     elif dir == 2:
-#         img[y1:ny1,x1:x2] = 0
-#     else:
-#         img[ny2:y2, x1:x2] = 0
-#     # M = np.float32([[1, 0, 20],
-#     #                 [0, 1, 0]])
-#     # img = cv.warpAffine(img, M, (img.shape[1], img.shape[0]))
-#     return img
 def move_image(self,dir):
     dx = [10,-10,0,0]
     dy = [0,0,10,-10]
